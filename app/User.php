4cbdf8ec9580f867b5193ab36f96bcc8a6f3d7fb
Fernando Neubaum <fernando@neubaum.com.ar>
2019-11-01 15:40:43 -0300

    Added continous integration improvements.

--- app/User.php
+++ app/User.php
@@ -1,17 +1,36 @@
 <?php
-
+/**
+ * Clase User que representa un usuario del sistema.
+ * php version 7
+ * 
+ * @category Class
+ * @package  App
+ * @author   Fernando Neubaum <fernando@neubaum.com.ar>
+ * @license  https://opensource.org/licenses/MIT MIT License
+ * @link     https://github.com/ifts16/ejemplo
+ */
 namespace App;
 
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
+/**
+ * Clase User que representa un usuario del sistema.
+ * php version 7
+ * 
+ * @category Class
+ * @package  App
+ * @author   Fernando Neubaum <fernando@neubaum.com.ar>
+ * @license  https://opensource.org/licenses/MIT MIT License
+ * @link     https://github.com/ifts16/ejemplo
+ */
 class User extends Authenticatable
 {
     use Notifiable;
 
     /**
-     * The attributes that are mass assignable.
+     * Atributos asignables en masa.
      *
      * @var array
      */
@@ -20,7 +39,7 @@ class User extends Authenticatable
     ];
 
     /**
-     * The attributes that should be hidden for arrays.
+     * Atributos ocultos..
      *
      * @var array
      */
@@ -29,7 +48,7 @@ class User extends Authenticatable
     ];
 
     /**
-     * The attributes that should be cast to native types.
+     * Atributos que tienen que ser conocidos a tipos nativos de PHP.
      *
      * @var array
      */
